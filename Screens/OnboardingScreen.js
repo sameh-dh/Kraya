4efb7de01a9e9f5e727211fd19c34adb103f9861
sameh-dh <samehdh44@gmail.com>
2023-10-28 19:10:58 +0100

    create react native app start with navigation

---
+++ Screens/OnboardingScreen.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {View , Text , Button , StyleSheet} from 'react-native';
+
+const LoginScreen = () => {
+    return (
+        <View style= {StyleSheet.container} >
+            <Text>Login Screen </Text>
+            <Button 
+            title= "Click Here"
+            onPress= {() => alert('Button clicked')}
+            />
+        </View>
+
+
+    );
+};
+
+export default LoginScreen ; 
+
+const styles = StyleSheet.create({
+    container : {
+        flex: 1 ,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
