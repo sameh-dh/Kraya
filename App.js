6b9c6eea1ea687fc2ce8a8d8974433804ff6f12a
sameh-dh <samehdh44@gmail.com>
2023-10-28 23:38:39 +0100

    the app will show the introduction only once

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator} from "@react-navigation/stack"
-
+import AsyncStorage from "@react-native-community/async-storage";
 import OnboardingScreen from "./Screens/OnboardingScreen"
 import LoginScreen from "./Screens/LoginScreen"
 
 const AppStack = createStackNavigator();
 export default function App() {
-  return (
-   <NavigationContainer>
+  const [isFirstLaunch , setIsFirstLaunch] = React.useState(null);
+
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLauched').then(value => {
+      if(value == null) {
+        AsyncStorage.setItem('alreadyLaunched',true);
+        setIsFirstLaunch(true)
+      } else {
+        setIsFirstLaunch(false)
+      }
+    })
+  },[])
+// check if the introduction page already seen by the user
+  if(isFirstLaunch === null) {
+    return null ; 
+   
+  }else if (isFirstLaunch === true) {
+    return (
+      <NavigationContainer>
     <AppStack.Navigator 
     headerMode="none" >
     <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
     <AppStack.Screen name="Login" component={LoginScreen} />
     </AppStack.Navigator>
    </NavigationContainer>
-  );
+    )
+  }else {
+   return  <LoginScreen />
+  }
+  
 }
 
--- Screens/OnboardingScreen.js
+++ Screens/OnboardingScreen.js
@@ -5,10 +5,11 @@ import Onboarding from  'react-native-onboarding-swiper' ;
 const OnboardingScreen = ({navigation}) => {
     return (
         <Onboarding 
-        onSkip={() => navigation.navigate("Login")}
-        onDone={() => navigation.navigate("Login")}
-        titleStyles={{ color: "#213263" ,fontSize:30 ,fontFamily:"Poppins",fontWeight:700}}
-        subTitleStyles = {{color:"#213263", }}
+     
+        onSkip={() => navigation.replace("Login")}
+        onDone={() => navigation.replace("Login")}
+        titleStyles={{ color: "#213263" ,fontSize:30 ,fontFamily: 'sans-serif',fontWeight:700}}
+        subTitleStyles = {{color:"#213263", lineHeight:22, fontWeight:700}}
         styles ={{color:"#ffe10b"}}
         imageContainerStyles={{
             width:100,  
@@ -21,20 +22,24 @@ const OnboardingScreen = ({navigation}) => {
                 style={{ height: 100, width: 100 }}
                 source={require('../assets/logo3.jpg')} />,
                 title : 'Welcome to krayti',
-                color : "blue",
-                subtitle: 'swipe to start'
+                subtitle: 'swipe to start',
+                subTitleStyles:{color:"#213263"}
             },
             { 
-                backgroundColor : '#fdeb93',
-                image : <Image source={require('../assets/onboarding-img2.png')} />,
-                title : 'Onboarding',
-                subtitle: 'hola'
+                backgroundColor : '#f9f4e9',
+                image : <Image 
+                style={{ height: 150, width: 150 }}
+                source={require('../assets/reading-book.png')} />,
+                
+                subtitle: 'Welcome to KRAYTI, the educational mobile app designed with utmost care for students aged 12 to 18.'
             },
             { 
-                backgroundColor : '#e9bcbe',
-                image : <Image source={require('../assets/onboarding-img3.png')} />,
-                title : 'Onboarding',
-                subtitle: 'hola'
+                backgroundColor : '#f9f4e9',
+                image : <Image 
+                style={{ height: 250, width: 250 }}
+                source={require('../assets/virtual-class.png')} />,
+               
+                subtitle: ' Our mission is to empower you to excel and reap the benefits of your educational journey. With KRAYTI, success is just a tap away.'
             }
         ]}
         />
