556cd4843f391acfa34b205d30137e142c670ee2
sameh-dh <samehdh44@gmail.com>
2023-10-28 22:12:36 +0100

    logo added

--- Screens/OnboardingScreen.js
+++ Screens/OnboardingScreen.js
@@ -5,16 +5,34 @@ import Onboarding from  'react-native-onboarding-swiper' ;
 const OnboardingScreen = ({navigation}) => {
     return (
         <Onboarding 
+        onSkip={() => navigation.navigate("Login")}
+        onDone={() => navigation.navigate("Login")}
+        titleStyles={{ color: "#213263" ,fontSize:30 ,fontFamily:"Poppins",fontWeight:700}}
+        subTitleStyles = {{color:"#213263", }}
+        styles ={{color:"#ffe10b"}}
+        imageContainerStyles={{
+            width:100,  
+        }}
         pages={[
+            
             { 
-                backgroundColor : '#fff',
-                image : <Image source={require('../assets/onboarding-img1.png')} />,
+                backgroundColor : '#f9f4e9',
+                image : <Image 
+                style={{ height: 100, width: 100 }}
+                source={require('../assets/logo3.jpg')} />,
                 title : 'Welcome to krayti',
+                color : "blue",
                 subtitle: 'swipe to start'
             },
             { 
-                backgroundColor : '#fff',
+                backgroundColor : '#fdeb93',
                 image : <Image source={require('../assets/onboarding-img2.png')} />,
+                title : 'Onboarding',
+                subtitle: 'hola'
+            },
+            { 
+                backgroundColor : '#e9bcbe',
+                image : <Image source={require('../assets/onboarding-img3.png')} />,
                 title : 'Onboarding',
                 subtitle: 'hola'
             }
